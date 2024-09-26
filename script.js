55b64f0e29c7289b9509688464ef5fc4ae183577
sanju <sanjutl19@gmail.com>
2024-09-26 11:36:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let backgrounds=document.querySelectorAll(".fading-images")
+    let headings=document.querySelectorAll(".fading-heading")
+    let imageIndex=0
+    function changeBackground(){
+      backgrounds[imageIndex].classList.remove("showing");
+      headings[imageIndex].classList.remove("show");
+      imageIndex++
+      if(imageIndex>=backgrounds.length){
+        imageIndex=0
+      }
+      backgrounds[imageIndex].classList.add("showing")
+      headings[imageIndex].classList.add("show")
+    }
+    setInterval(changeBackground,3000)
+  
+  })
